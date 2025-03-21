c190d882f8640cfec7cdd45b7ef1132f219c19e5
Soth-kimleng <ksoth@paragoniu.edu.kh>
2025-03-21 16:47:14 +0700

    initial commit

---
+++ src/main.tsx
@@ -0,0 +1,35 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from './pages/home.tsx';
+import Favorites from './pages/favorites.tsx';
+import Layout from './components/layout/index.tsx';
+import NotFound from './pages/notFound.tsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: 'favorites',
+        element: <Favorites />,
+      },
+      {
+        path: '*',
+        element: <NotFound />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
